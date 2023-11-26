02324f4bf5d4b3a398348edeea50d59938d169e4
ahadhalloul <ahadhalloul@hotmail.com>
2023-11-26 11:31:18 +0300

    add tablet design for all products home page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// ---------------------------------
+// adding search wrapper
 let searchWrapper = document.getElementById("searchWrapper");
 let searchButton = document.getElementById("searchButton");
 let searchBar = document.getElementById("searchBar");
@@ -6,13 +8,50 @@ searchButton.addEventListener("click", (e) => {
   searchWrapper.classList.add("active");
 });
 
-// searchWrapper.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   searchWrapper.classList.remove("active");
-// });
-
 searchWrapper.addEventListener("click", function (e) {
   if (!searchBar.contains(e.target)) {
     searchWrapper.classList.remove("active");
   }
 });
+
+// -----------------------------------------
+// show and hide navigation div in small screen
+let menuIcon = document.querySelector(".nav-menu-icon");
+let navList = document.querySelector(".nav-list");
+let body = document.querySelector("body");
+let bodyBg = document.querySelector(".body-bg");
+let logo = document.querySelector(".logo-container");
+menuIcon.addEventListener("click", function () {
+  body.classList.add("stop-scrolling");
+  logo.classList.add("logo-inside-menu");
+  navList.classList.add("show-flex");
+  bodyBg.classList.remove("hide");
+});
+
+bodyBg.addEventListener("click", function (e) {
+  if (!navList.contains(e.target)) {
+    body.classList.remove("stop-scrolling");
+    logo.classList.remove("logo-inside-menu");
+    navList.classList.remove("show-flex");
+    bodyBg.classList.add("hide");
+  }
+});
+
+// ---------------------------------------
+// add scroll buttons of price table plan
+let priceTable = document.querySelector(".pricing-table");
+const distance = 340;
+
+function scrollLft() {
+  priceTable.scrollBy({
+    left: -distance,
+    behavior: "smooth",
+  });
+}
+
+function scrollRight() {
+  priceTable.scrollBy({
+    left: distance,
+    behavior: "smooth",
+  });
+}
